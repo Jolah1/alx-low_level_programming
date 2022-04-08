84ee3141bac15236f40f74282c4bd177dcd4e65c
jolah1 <jokunlade@gmail.com>
2022-04-08 08:51:27 -0700

    adding alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+char a;
+for (a = 'z'; a >= 'a' ; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
