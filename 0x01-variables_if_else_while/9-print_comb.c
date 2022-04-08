4868b0e4cfc874d9e83dba81a2c68a32442a5e59
jolah1 <jokunlade@gmail.com>
2022-04-08 09:10:49 -0700

    done with mandatory

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int i;
+for (i = 10; i < 20; i++)
+{
+putchar((i % 10) + '0');
+if (i != 19)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
