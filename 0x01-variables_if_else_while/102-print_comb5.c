2b905e9f0000983e0afba13436989d1bca4dcd40
jolah1 <jokunlade@gmail.com>
2022-04-08 10:30:22 -0700

    final task correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,14 +15,12 @@ for (j = i + 1; j <= 99; j++)
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
-putchar(j / 10 + '0');								putchar(j % 10 + '0');
-if (i == 98)
-{
-if (j == 99)
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (!(i == 98 && j == 99))
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
