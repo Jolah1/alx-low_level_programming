d3cf33436495a6c6c345948880bddb52d4c5ddeb
jolah1 <jokunlade@gmail.com>
2022-04-08 08:42:23 -0700

    adding numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10 ; a++)
+putchar('0' + a);
+putchar('\n');
+return (0);
+}
