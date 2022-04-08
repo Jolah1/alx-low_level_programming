38a811069d73ed7bf85650cfd2857ef8c3c81b40
jolah1 <jokunlade@gmail.com>
2022-04-08 08:09:14 -0700

    corrections again

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,10 +5,12 @@
  * Return: always return 0
  */
 int main(void)
+{
 char a;
 for (a = 'a'; a <= 'z' ; a++)
 putchar(a);
 for (a = 'A'; a <= 'Z'; a++)
-putchar(a);
-putchar('\n');
+putchar (a);
+putchar ('\n');
 return (0);
+}
