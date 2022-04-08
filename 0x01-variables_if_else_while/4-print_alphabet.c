fb8ac8540887e8b8b1a974cb57dcf1371182ae9f
jolah1 <jokunlade@gmail.com>
2022-04-08 08:22:27 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+{
+if (a == 'q' || a == 'e')
+{
+continue;
+}
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
