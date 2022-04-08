4dee49ba6e8243f46ce89a9b2137d5050eb8a520
jolah1 <jokunlade@gmail.com>
2022-04-08 08:35:59 -0700

    another printing alphabet

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10 ; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
