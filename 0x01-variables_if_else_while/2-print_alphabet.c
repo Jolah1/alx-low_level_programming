5f42985741da245c4ee3cff6839384232761a375
jolah1 <jokunlade@gmail.com>
2022-04-08 07:24:11 -0700

    another task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
