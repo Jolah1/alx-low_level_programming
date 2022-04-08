6613d72e2b89ef7bb139e2eb3b112eb45809da8a
jolah1 <jokunlade@gmail.com>
2022-04-08 09:48:25 -0700

    finally done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');								putchar(j % 10 + '0');
+if (!(i == 98 && j == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
