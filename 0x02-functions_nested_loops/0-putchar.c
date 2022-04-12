5f356b120d997719f68b4e81b54c355f4cf9e85a
jolah1 <jokunlade@gmail.com>
2022-04-12 04:30:42 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putcher.h"
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('e');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Functions, nested and loops in c programming
