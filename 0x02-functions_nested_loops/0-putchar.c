084ee7c5316300d05294079f9c6c8ff36ed97828
jolah1 <jokunlade@gmail.com>
2022-04-12 07:47:53 -0700

    original first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,16 @@
  * main - entry point
  * Return: always return 0
  */
-int_putchar(char c)
+int main(void)
 {
-char *sh = "_putcher";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
