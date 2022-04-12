4ddce7587353c23c92787e7bb823b26769bdd97f
jolah1 <jokunlade@gmail.com>
2022-04-12 10:09:44 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+int i;
+int j;
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
+}
+}
+}
