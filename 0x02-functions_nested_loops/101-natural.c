a1091091be13e8f30a2b3c3e91e8958154a84516
jolah1 <jokunlade@gmail.com>
2022-04-12 11:45:10 -0700

    natural c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always return 0
+ */
+int main(void)
+{
+int a;
+int b = 0;
+for (a = 0; a < 1024; a++)
+{
+if (a % 3 == 0 || a % 5 == 0)
+{
+b += a;
+}
+}
+printf("%d\n", b);
+return (0);
+}
