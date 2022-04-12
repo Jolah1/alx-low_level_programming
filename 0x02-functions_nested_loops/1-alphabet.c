816ca866c551b7511c0c9e16909c460a77dd0f55
jolah1 <jokunlade@gmail.com>
2022-04-12 08:09:01 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
