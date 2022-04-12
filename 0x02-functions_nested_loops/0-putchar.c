ad62e639d79fa9964237bb0d8e6bc29f23713e2a
jolah1 <jokunlade@gmail.com>
2022-04-12 06:14:25 -0700

    still first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('e');
-_putchar('r');
+char *sh = "_putcher";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 return (0);
 }
