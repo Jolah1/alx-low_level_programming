04d50f6d902d81d5a55a83b1143ba528e835bfa2
jolah1 <jokunlade@gmail.com>
2022-04-13 03:02:36 -0700

    my first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - test function for positive or negative
+ * Return: always return 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
