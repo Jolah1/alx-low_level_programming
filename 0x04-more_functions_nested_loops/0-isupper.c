6338398fe5feb5ede32c12ed0688c664a261a309
jolah1 <jokunlade@gmail.com>
2022-04-14 04:11:37 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isupper - checks if parameter c represents an upper case
+ * @c: int representing a character
+ * Return: always return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
