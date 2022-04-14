52f972dbc59ae31d9ddd565dbc82f3ff27887dbe
jolah1 <jokunlade@gmail.com>
2022-04-14 08:12:11 -0700

    last mandatory

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.>
+
+/**
+ * main - entry point
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (; i < 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+
+	return (0);
+}
