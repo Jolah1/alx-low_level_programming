d6235006b1f0f0202b10234c491ff189f96ad6c2
jolah1 <jokunlade@gmail.com>
2022-04-20 14:29:29 -0700

    still adding

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Entry Point
+ * @s: Varaible
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar("\n");
+}
