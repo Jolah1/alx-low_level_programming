423715aa8465c7f5983f0b181505d18929469cea
jolah1 <jokunlade@gmail.com>
2022-04-20 12:55:02 -0700

    new commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,19 @@
 #include "main.h"
+
 /**
- * swap_int - swap variable values 
- * @a: pointer 1 
- * @b: pointer 2 
- * Return: void 
-*/
+ * swap_int - swap variable values
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: always return 0
+ */
 
 void swap_int(int *a, int *b)
 {
-	*a + = *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	int x, y;
+
+	x = *a;
+	y = *b;
+
+	*a = y;
+	*b = x;
 }
