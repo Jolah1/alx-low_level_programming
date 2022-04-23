855721bce7745bbcfa7c4e93c335f864b0600910
jolah1 <jokunlade@gmail.com>
2022-04-23 11:29:00 -0700

    first advance

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @s: input variables
+ * Return: alwyas return dest
+ */
+
+char *rot13*char *s)
+{
+	int count = 0, x;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
