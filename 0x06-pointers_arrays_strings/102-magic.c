f8f4eddb731fda27770972b3116042bc3bfe78d7
jolah1 <jokunlade@gmail.com>
2022-04-24 03:51:13 -0700

    grit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to write more than this line of code
    */
-  p[5] = 98;
+ p[5] = 98;
   /* .....so that it prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
