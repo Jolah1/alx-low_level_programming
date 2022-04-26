6f83c13f439c825884a1494225750f03c7d1c212
jolah1 <jokunlade@gmail.com>
2022-04-26 11:51:16 -0700

    adding diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of diagonals
+ * @a: string
+ * @size: size of square
+ * Return: always return 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int x, y, sum1, sum2;
+	sum1 = 0;
+	sum2 = 0;
+
+	for (x =0; x < size; x++)
+
+		sum1 += a[(size + 1) * x];
+
+	for (y = 0; y < size; y++)
+
+		sum2 += a[(size - 1) * (y + 1)];
+
+	printf("%d, %d\n", sum1, sum2);
+}
