6dbc3bf62f4ae2c3eac20eafefabbfd86db95e51
jolah1 <jokunlade@gmail.com>
2022-04-26 07:29:19 -0700

    first task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,14 @@
  * @s: space of the memory
  * @b: bytes
  * @n: memory area
+ *
  * Return: return ponter to s string
  */
 
-char *_memset(char *s, char b, unsighned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int x = 0;
+	unsigned int x;
+	x = 0;
 
 	while (x < n)
 	{
