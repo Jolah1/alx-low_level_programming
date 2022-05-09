b954ceb8d0bc114324e186622ca3e9ac1138eedc
jolah1 <jokunlade@gmail.com>
2022-05-09 11:46:22 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,19 +10,19 @@
 int main(int argc, char *argv[])
 {
 	unsigned long mul;
-	int i, j;
+	int num1, num2;
 
 	if (argc != 3)
 	{ printf("Error\n");
 		exit(98); }
 
-	for (i = 1; i < argc; i++)
+	for (num1 = 1; num1 < argc; num1++)
 	{
 
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
 
-			if (argv[i][j] > 57 || argv[i][j] < 48)
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
 			{  printf("Error\n");
 				exit(98); }
 		}
