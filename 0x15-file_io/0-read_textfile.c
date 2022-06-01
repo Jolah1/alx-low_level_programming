7bc15345d743089b9d3e29d0d75142954ebad87d
jolah1 <jokunlade@gmail.com>
2022-06-01 10:54:08 -0700

    adding more commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * * read_textfile - Reads a text file and prints it
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters to read and print
+ * Return: return actual number of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(op);
+
+	return (wr);
+}
