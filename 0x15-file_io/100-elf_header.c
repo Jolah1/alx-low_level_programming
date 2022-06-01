fb6fc59bd681434a91e92d4ce5db05240ff5a599
jolah1 <jokunlade@gmail.com>
2022-06-01 14:49:29 -0700

    more correctionss

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -77,6 +77,9 @@ void print_class(unsigned char *e_ident)
 		case ELFCLASS32:
 			printf("ELF32\n");
 			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
 		default:
 			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
